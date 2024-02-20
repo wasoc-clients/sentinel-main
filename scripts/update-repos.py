b7d321e9e00727a7887fb42e5328cf5b150efb90
Adon Metcalfe <adonm@fastmail.fm>
2024-02-20 03:02:05 +0000

    basics of copy content around

--- scripts/update-repos.py
+++ scripts/update-repos.py
@@ -2,9 +2,11 @@
 from subprocess import run
 import json
 
-repos = json.loads(run(["gh", "repo", "list", "wasoc-clients", "--json", "url"], capture_output=True).stdout.decode("utf8"))
+repos = json.loads(run(["gh", "repo", "list", "wasoc-clients", "--json", "name"], capture_output=True).stdout.decode("utf8"))
 for repo in repos:
-    url = repo["url"]
-    if url.endswith("-main"):
+    name = repo["name"]
+    if name.endswith("-main") or not name.startswith("sentinel-"): # Ignore main template repo and any repo not starting with `sentinel-`
         continue
-    print(url)
+    run(["gh", "repo", "clone", f"wasoc-clients/{name}", f"target-repos/{name}"]) # should only need to run the first time
+    run(["rsync", "-avr", "--exclude", ".git", "templates/base/", f"target-repos/{name}/"]) # copy over updated template stuff
+    run(f"cd target-repos/{name} && git add . && git commit -am 'updated content' && git push", shell=True) # commit and push changes (probs needs better error logging)
